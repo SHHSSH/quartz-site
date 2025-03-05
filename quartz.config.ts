3ab41d76171758f3949a568866f7acfcf3562fbf
SHH, quiet... this will destroy you. <21285581+SHHSSH@users.noreply.github.com>
2025-03-05 20:49:24 +1000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "shhssh.github.io/quartz-site",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
